9c68c10f0902b5c823900ddcc6774fe84f7ce646
Mikhail Shkaralevich <mkaralevich8@gmail.com>
2024-01-20 15:53:56 -1000

    Add class to store prop types

---
+++ app/imports/api/propTypes/PropTypes.js
@@ -0,0 +1,13 @@
+import PropTypes from 'prop-types';
+
+const PropTypeNonprofit = PropTypes.shape({
+  type: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  mission: PropTypes.string.isRequired,
+  contactInfo: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  createdAt: PropTypes.instanceOf(Date).isRequired,
+  picture: PropTypes.string,
+});
+
+export { PropTypeNonprofit };
