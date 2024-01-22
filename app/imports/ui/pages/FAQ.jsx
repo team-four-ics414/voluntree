73d2786eaa9a3fe96e0b2236722ab8d0984985f8
mnakagawa14 <134327026+mnakagawa14@users.noreply.github.com>
2024-01-22 11:38:30 -1000

    Changed placeholder text

--- app/imports/ui/pages/FAQ.jsx
+++ app/imports/ui/pages/FAQ.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Col, Container, Image, Row } from 'react-bootstrap';
-import { PAGE_IDS } from '../utilities/PageIDs';
+import { Col, Container, Row } from 'react-bootstrap';
 import Accordion from 'react-bootstrap/Accordion';
+import { PAGE_IDS } from '../utilities/PageIDs';
 
 const FAQ = () => (
   <Container id={PAGE_IDS.FAQ} className="py-3">
@@ -12,25 +12,13 @@ const FAQ = () => (
           <Accordion.Item eventKey="0">
             <Accordion.Header>Question #1?</Accordion.Header>
             <Accordion.Body>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-              minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-              aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-              culpa qui officia deserunt mollit anim id est laborum.
+              Answer #1
             </Accordion.Body>
           </Accordion.Item>
           <Accordion.Item eventKey="1">
             <Accordion.Header>Question #2?</Accordion.Header>
             <Accordion.Body>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-              minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-              aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-              culpa qui officia deserunt mollit anim id est laborum.
+              Answer #2
             </Accordion.Body>
           </Accordion.Item>
         </Accordion>
