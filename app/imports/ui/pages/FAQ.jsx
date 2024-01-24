813227ac8973ae46468a20db2c8c33975946a1ae
mnakagawa14 <134327026+mnakagawa14@users.noreply.github.com>
2024-01-24 03:23:27 -1000

    Updated Example Questions and Answers

--- app/imports/ui/pages/FAQ.jsx
+++ app/imports/ui/pages/FAQ.jsx
@@ -10,15 +10,35 @@ const FAQ = () => (
         <h1>FAQ</h1>
         <Accordion defaultActiveKey="0">
           <Accordion.Item eventKey="0">
-            <Accordion.Header>Question #1?</Accordion.Header>
+            <Accordion.Header>How can I find volunteering opportunities that best suit my skills?</Accordion.Header>
             <Accordion.Body>
-              Answer #1
+              You can browse our website and use the search feature to find volunteering opportunities near you. Filter results based on your interests, skills, and availability.
             </Accordion.Body>
           </Accordion.Item>
           <Accordion.Item eventKey="1">
-            <Accordion.Header>Question #2?</Accordion.Header>
+            <Accordion.Header>What types of volunteer work are available?</Accordion.Header>
             <Accordion.Body>
-              Answer #2
+              We work with a diverse range of volunteer groups, including but not limited to environmental conservation, community service, education, and healthcare. Explore our categories to find a cause that resonates with you.
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="2">
+            <Accordion.Header>How do I sign up to volunteer?</Accordion.Header>
+            <Accordion.Body>
+              To sign up, create an account on our website, browse available opportunities, and click on &quot;Apply&quot; for the ones you&apos;re interested in.
+              Follow the instructions provided by the organization to complete the application process.
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="3">
+            <Accordion.Header>What age restrictions apply to volunteering?</Accordion.Header>
+            <Accordion.Body>
+              The age requirements can vary depending on the opportunity and organization. Check the details of each opportunity for specific age restrictions.
+              Some opportunities may be suitable for families with children, while others may have a minimum age requirement.
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="4">
+            <Accordion.Header>How can I track my volunteer hours?</Accordion.Header>
+            <Accordion.Body>
+              Once you&apos;ve volunteered, log in to your account and navigate to the &quot;My Profile&quot; or &quot;Dashboard&quot; section. You can view and track your volunteer hours there.
             </Accordion.Body>
           </Accordion.Item>
         </Accordion>
