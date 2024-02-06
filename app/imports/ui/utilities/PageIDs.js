2dc55926d32315d18503bc33743b74cd51e00664
Joel Vanta <122860229+joelvanta@users.noreply.github.com>
2024-02-06 10:38:35 -1000

    Updated FAQ footer link
    Footer 'FAQ' link to FAQ.jsx

--- app/imports/ui/utilities/PageIDs.js
+++ app/imports/ui/utilities/PageIDs.js
@@ -11,6 +11,7 @@ export const PAGE_IDS = {
   SIGN_IN: 'sign-in',
   SIGN_OUT: 'sign-out',
   SIGN_UP: 'sign-up',
+  FAQ: 'faq',
   CHAT: 'chat',
   ADD_NONPROFIT: 'add-nonprofit',
   EDIT_NONPROFIT: 'edit-nonprofit',
