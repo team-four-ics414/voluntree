842e598660ea035541a1ff9b5856aa1b8afef660
mnakagawa14 <134327026+mnakagawa14@users.noreply.github.com>
2024-02-07 08:23:34 -1000

    Added links and standardized style

--- app/imports/ui/pages/FAQ.jsx
+++ app/imports/ui/pages/FAQ.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import Accordion from 'react-bootstrap/Accordion';
-import { Link } from 'react-router-dom';
 import { PAGE_IDS } from '../utilities/PageIDs';
+
+// Need to update link once search features are implemented
+// -Search
+// -Dashboard
 
 const FAQ = () => (
   <Container id={PAGE_IDS.FAQ} className="py-3">
@@ -13,7 +16,7 @@ const FAQ = () => (
           <Accordion.Item eventKey="0">
             <Accordion.Header>How can I find volunteering opportunities that best suit my skills?</Accordion.Header>
             <Accordion.Body>
-              You can browse our website and use the search feature to find volunteering opportunities near you. Filter results based on your interests, skills, and availability.
+              You can browse our website and use <a href="./">Search</a> to find volunteering opportunities near you. Filter results based on your interests, skills, and availability.
             </Accordion.Body>
           </Accordion.Item>
           <Accordion.Item eventKey="1">
@@ -25,10 +28,8 @@ const FAQ = () => (
           <Accordion.Item eventKey="2">
             <Accordion.Header>How do I sign up to volunteer?</Accordion.Header>
             <Accordion.Body>
-              To sign up, create an account on our website, browse available opportunities, and click on &quot;Apply&quot; for the ones you&apos;re interested in.
+              To sign up, <a href="./signup">create an account</a> an account on our website, browse available opportunities, and click on &quot;Apply&quot; for the ones you&apos;re interested in.
               Follow the instructions provided by the organization to complete the application process.
-              <div> <Link to="/signup"> Click here to register for an account.</Link> </div>
-
             </Accordion.Body>
           </Accordion.Item>
           <Accordion.Item eventKey="3">
@@ -41,7 +42,7 @@ const FAQ = () => (
           <Accordion.Item eventKey="4">
             <Accordion.Header>How can I track my volunteer hours?</Accordion.Header>
             <Accordion.Body>
-              Once you&apos;ve volunteered, log in to your account and navigate to the &quot;My Profile&quot; or &quot;Dashboard&quot; section. You can view and track your volunteer hours there.
+              Once you&apos;ve volunteered, log in to your account and navigate to the <a href="./">My Profile</a>  or <a href="./">Dashboard</a>  section. You can view and track your volunteer hours there.
             </Accordion.Body>
           </Accordion.Item>
         </Accordion>
