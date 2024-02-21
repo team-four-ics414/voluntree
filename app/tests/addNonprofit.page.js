62ed62c553e3de58558d7567f60a07acd249a724
Mikhail Shkaralevich <mkaralevich8@gmail.com>
2024-02-20 17:58:32 -1000

    Adjust unit test

--- app/tests/addNonprofit.page.js
+++ app/tests/addNonprofit.page.js
@@ -11,8 +11,9 @@ class AddNonprofitPage {
     this.name = Selector('#add-nonprofit [name="name"]');
     this.mission = Selector('#add-nonprofit [name="mission"]');
     this.contactInfo = Selector('#add-nonprofit [name="contactInfo"]');
-    this.picture = Selector('#add-nonprofit [name="picture"]');
-    this.pictureUrl = 'https://banner2.cleanpng.com/20180628/eoy/kisspng-charitable-organization-habitat-for-humanity-compu-commercial-business-card-5b3509ef581089.6657184215302026073607.jpg';
+    this.location = Selector('#add-nonprofit [name="location"]');
+    // this.picture = Selector('#add-nonprofit [name="picture"]');
+    // this.pictureUrl = 'https://banner2.cleanpng.com/20180628/eoy/kisspng-charitable-organization-habitat-for-humanity-compu-commercial-business-card-5b3509ef581089.6657184215302026073607.jpg';
   }
 
   /** Checks that this page is currently displayed. */
@@ -24,12 +25,13 @@ class AddNonprofitPage {
   async addNonprofit() {
     await t
       .click(this.type)
-      .click(this.type.find('option').withText('Organization'))
-      .expect(this.type.value).eql('Organization')
+      .click(this.type.find('option').withText('Schools'))
+      .expect(this.type.value).eql('Schools')
       .typeText(this.name, 'Caring Hearts')
       .typeText(this.mission, 'Help other people.')
       .typeText(this.contactInfo, '808-808-8080')
-      .typeText(this.picture, this.pictureUrl)
+      .typeText(this.location, 'Kailua, HI')
+      // .typeText(this.picture, this.pictureUrl)
       .click(Selector(`#${COMPONENT_IDS.NONPROFIT_ADD_SUBMIT_BTN} input`))
       .click(Selector('button').withText('OK'));
   }
