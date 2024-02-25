ea338503708cbf99a2f1e9b1b21aba16fb58691c
ThanhLy1 <123609101+ThanhLy1@users.noreply.github.com>
2024-02-25 13:42:58 -1000

    Added Bootstrap Alert

--- app/imports/ui/components/calendar/EventModal.jsx
+++ app/imports/ui/components/calendar/EventModal.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Modal, Button, Form } from 'react-bootstrap';
+import { Modal, Button, Form, Alert } from 'react-bootstrap';
 import DatePicker from 'react-datepicker';
 import PropTypes from 'prop-types';
 import 'react-datepicker/dist/react-datepicker.css';
