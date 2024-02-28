4a9e3232a49674aa83a69e2910c188b6e15bc1ef
gavyngostage <ggostage@hawaii.edu>
2024-02-28 09:24:57 -1000

    Added button, still in progress

--- app/imports/ui/components/forum/ForumPostCard.jsx
+++ app/imports/ui/components/forum/ForumPostCard.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import { Card } from 'react-bootstrap';
+import { Card, Button } from 'react-bootstrap';
 
 const ForumPostCard = ({ post }) => {
   return (
     <Card style={{margin: "10px 15px 10px 15px", maxWidth: "96%"}}>
       <Card.Body>
         <Card.Title>In need of Dog sitter</Card.Title>
-        <Card.Text>I need a dog sitter please please please. I need a dog sitter please please please. I need a dog sitter please please please. I need a dog sitter please please please. I need a dog sitter please please please. I need a dog sitter please please please.</Card.Text>
+        <Card.Text>I need a dog sitter please please please. I need a dog sitter please please please. I need a dog sitter please please please. I need a dog sitter please please please. I need a dog sitter please please please. I need a dog sitter please please please.
+        </Card.Text>
+        <Card.Footer style={{ display: 'flex', justifyContent: 'flex-end' }}>
+          <Button variant="success">I'm Interested</Button>
+        </Card.Footer>
       </Card.Body>
     </Card>
   );
