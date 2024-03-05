db5f5657eabe9bcb9a85baa7aae2eb72dc4cf1bf
gavyngostage <ggostage@hawaii.edu>
2024-03-05 01:13:57 -1000

    still working on forum page
    having a lot of hardware issues.

--- app/imports/ui/components/forum/ForumPostCard.jsx
+++ app/imports/ui/components/forum/ForumPostCard.jsx
@@ -1,18 +1,30 @@
 import React from 'react';
-import { Card, Button } from 'react-bootstrap';
+import { Card, Button, Accordion } from 'react-bootstrap';
 
 const ForumPostCard = ({ post }) => {
   return (
-    <Card style={{margin: "10px 15px 10px 15px", maxWidth: "96%"}}>
-      <Card.Body>
-        <Card.Title>In need of Dog sitter</Card.Title>
-        <Card.Text>I need a dog sitter please please please. I need a dog sitter please please please. I need a dog sitter please please please. I need a dog sitter please please please. I need a dog sitter please please please. I need a dog sitter please please please.
-        </Card.Text>
-        <Card.Footer style={{ display: 'flex', justifyContent: 'flex-end' }}>
+    <Accordion defaultActiveKey="0">
+      <Card style={{margin: "10px 15px 10px 15px", maxWidth: "96%"}}>
+        <Card.Body>
+          <Card.Title>In need of Dog sitter</Card.Title>
+          <Card.Text>I need a dog sitter please please please. I need a dog sitter please please please. I need a dog sitter please please please. I need a dog sitter please please please. I need a dog sitter please please please. I need a dog sitter please please please.
+          </Card.Text>
+          <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
           <Button variant="success">I'm Interested</Button>
-        </Card.Footer>
-      </Card.Body>
-    </Card>
+          </div>
+        </Card.Body>
+        <Accordion defaultActiveKey="1">
+          <Accordion.Button eventKey="0">Read more</Accordion.Button>
+          <Accordion.Collapse eventKey="0">
+            <Card.Body>
+              <Card.Text>
+              BLAH BLAH BLAH BLAH
+              </Card.Text>
+            </Card.Body>
+          </Accordion.Collapse>
+        </Accordion>
+      </Card>
+    </Accordion>
   );
 };
 
