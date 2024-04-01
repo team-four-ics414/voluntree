1c61bc0ae16faebe1c726332bc51e48fe06f03f6
Joel Vanta <122860229+joelvanta@users.noreply.github.com>
2024-04-01 09:51:40 -1000

    UserProfile page update

--- app/imports/ui/pages/UserProfile.jsx
+++ app/imports/ui/pages/UserProfile.jsx
@@ -3,6 +3,7 @@ import { Col, Container, Row } from 'react-bootstrap';
 import { PAGE_IDS } from '../utilities/PageIDs';
 import ProfilesDisplay from '../components/profile/ProfileCardDisplay';
 import ProfileList from '../components/profile/ProfileList';
+import Calendar from '../components/calendar/Calendar';
 
 /** Render a Not Found page if the user enters a URL that doesn't match any route. */
 const UserProfilePage = () => (
@@ -13,7 +14,12 @@ const UserProfilePage = () => (
           <p>User Profile</p>
         </h2>
         {/*<ProfileList />*/}
-        <ProfilesDisplay />
+        <ProfilesDisplay/>
+        <h2>
+          <p>Upcoming Events</p>
+        </h2>
+
+        <Calendar/>
 
       </Col>
     </Row>
